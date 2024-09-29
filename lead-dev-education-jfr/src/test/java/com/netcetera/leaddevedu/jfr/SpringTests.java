a6e210bdf0f49c2ee1a9bd3dcb743cb0fb7a739b
Philippe Marschall <philippe.marschall@gmail.com>
2024-09-29 21:17:11 +0200

    fix jfr

--- lead-dev-education-jfr/src/test/java/com/netcetera/leaddevedu/jfr/SpringTests.java
+++ lead-dev-education-jfr/src/test/java/com/netcetera/leaddevedu/jfr/SpringTests.java
@@ -1,8 +1,12 @@
 package com.netcetera.leaddevedu.jfr;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.metrics.jfr.FlightRecorderApplicationStartup;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
@@ -12,14 +16,22 @@ import com.netcetera.leaddevedu.jfr.SpringTests.UseFlightRecorderApplicationStar
 @SpringJUnitConfig(initializers = UseFlightRecorderApplicationStartup.class)
 class SpringTests {
 
-  @Test
-  void simpleTest() {
+  @Autowired
+  private SlowBean slowBean;
 
+  @Test
+  void simpleTest() throws InterruptedException {
+    assertNotNull(this.slowBean);
   }
 
   @Configuration
   static class SpringConfiguration {
 
+    @Bean
+    public SlowBean slowBean() throws InterruptedException {
+      return new SlowBean();
+    }
+    
   }
 
   static class UseFlightRecorderApplicationStartup implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -27,6 +39,14 @@ class SpringTests {
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
       applicationContext.setApplicationStartup(new FlightRecorderApplicationStartup());
+    }
+
+  }
+
+  static class SlowBean {
+
+    SlowBean() throws InterruptedException {
+      Thread.sleep(100L);
     }
 
   }
