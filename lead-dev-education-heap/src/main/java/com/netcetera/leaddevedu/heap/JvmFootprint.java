4ca86e169287fe9fa313c021f8bcc214ef333fe8
Philippe Marschall <philippe.marschall@gmail.com>
2024-11-13 20:03:44 +0100

    module for heap footprint

---
+++ lead-dev-education-heap/src/main/java/com/netcetera/leaddevedu/heap/JvmFootprint.java
@@ -0,0 +1,11 @@
+package com.netcetera.leaddevedu.heap;
+
+import java.io.IOException;
+
+public class JvmFootprint {
+
+  public static void main(String[] args) throws IOException {
+    System.in.read();
+  }
+
+}
